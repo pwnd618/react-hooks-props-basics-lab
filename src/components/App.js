d23fba6da8aa48ac2f23a93a86b14d911134a6ea
webster <wbian618@gmail.com>
2021-11-06 14:08:18 -0400

    commit tree 05d3fd56199d436dc43ead9f2f38d35e3b59dbec

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
-
+// console.log(user);
+// console.log(user.links.github)
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About links={user.links} bio={user.bio} /> 
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Home(props) {
+  // console.log(props)
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.username} is a Web Developer from {props.city}
       </h1>
     </div>
-  );
+    );
 }
 
 export default Home;
